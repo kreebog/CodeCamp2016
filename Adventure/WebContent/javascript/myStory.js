304c852c6d61845f2b777fc43cc985669026a095
kreebog <kreebog@gmail.com>
2016-06-25 21:12:27 -0400

    minor updates

--- Adventure/WebContent/javascript/myStory.js
+++ Adventure/WebContent/javascript/myStory.js
@@ -9,8 +9,9 @@ var myStory = {
 		{
 			"id": 0,
 			"title": "On the High Seas",
+			"author": "James Dean",
 			"text": 
-					"The sun hangs directly overhead, as bright and hot as the tip of an iron sabre fresh from the forge.  The compass points due north, but your first mate, Duncan, tells you that you're running low on fresh water and that the deckhands are starting to complain.  <br><br>'Turn east, Captain,' he says.  Land is but a half-day stroll with this strong west wind at our backs.'",
+					"The sun hangs directly overhead, as bright and hot as the tip of an iron sabre fresh from the forge.  The ship's compass points due north, but your first mate, Duncan, tells you that you're running low on fresh water and that the crew is starting to worry.  <br><br>'Turn east, Captain,' he says.  Land is but a half-day's sail with this strong west wind at our backs.  We'll find water for sure... and maybe something better!'",
 			"choices" : [  
 				{"id": 1, "text": "Head East", "nextChapterId": 1},
 				{"id": 2, "text": "Keep Sailing North", "nextChapterId": 2},
@@ -21,6 +22,7 @@ var myStory = {
 		{
 			"id": 1,
 			"title": "Heading East",
+			"author": "Jimmy Dean",
 			"text": 
 					"You order the helmsman to turn right and head east as the brutally hot sun slowly starts to drop behind you.  True to Duncan's word, the cry of 'Land Ho!' rings out from the crow's nest just before the sun touches the water.  <br><br>Squinting through your foggy telescope, you see a lush, green coastline edged with glimmering beaches and what appears to be a city made of gold.",
 			"choices" : [  
